350e4d59e997e3fec978e8155cf9b2be8f4377e2
JulianaAbreu <juliana.cerqueira55@gmail.com>
2019-11-03 18:52:36 -0300

    added text phrase dialog

--- interfaces/Dialog.py
+++ interfaces/Dialog.py
@@ -1,15 +1,15 @@
 import pygame
 from interfaces.BaseScreen import BaseScreen
-# from components.Fonts import text
+from components.Fonts import text
+from components.Color import level_colors
 from components.Images import backgrounds
 
 class Dialog(BaseScreen):
     def __init__(self):
-        # red = 212, 66, 66
-        # size = width, height = 600, 400
-        # screen = pygame.display.set_mode(size)
-        # screen.fill(red)
-
-        # self.mounted_screen = self.base_screen.copy()
-        
         super().__init__(backgrounds["dialogs"])
+        phrase = "Olá!! Meu nome é Josh! Acabei de chegar de várias viagens que fiz a todos os continentes! Conheci o Brasil, a França, a Austrália, a África, e até mesmo a China."
+
+        phrase_wrapper = text(phrase, "asap/regular.ttf", 30, "#484848", (800, 140))
+        posX = 50
+        posY = 410
+        self.mounted_screen.blit(phrase_wrapper,  (posX, posY))
