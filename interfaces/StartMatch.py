3a5a3469b4c44edf5a145795f6721646da063fba
Douglas Martins <douglas.martins.m099@gmail.com>
2019-11-07 10:57:53 -0300

    Replace Josh Background; Fix Words Amound in Start Screen

--- interfaces/StartMatch.py
+++ interfaces/StartMatch.py
@@ -12,12 +12,13 @@ class StartMatch(BaseScreen):
         self.basicElements()
 
     def basicElements(self):
+        words_amount = len(self.session.crossword.structure)
         start_btn = buttons["start_match"].copy()
         start_label = text("Iniciar Partida", "asap/regular.ttf", 18, "#FFFFFF", (120, 25))
         start_btn.blit(start_label, (53, 12))
 
         country_label = text("País: " + self.session.country["name"], "asap/semibold.ttf", 27, "#373737", (220, 35))
-        words_label = text(str(self.session.level * 3) + " palavras ocultas", "asap/italic.ttf", 20, "#5E5D5D", (220, 35))
+        words_label = text(str(words_amount) + " palavras ocultas", "asap/italic.ttf", 20, "#5E5D5D", (220, 35))
 
         self.base_screen.blit(badges[self.session.level - 1], (295, 55))
         self.base_screen.blit(country_label, (247, 448))
